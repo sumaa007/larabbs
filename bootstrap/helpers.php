31b54265eed0dc20dd694a5af3a618115564f444
sumaa007 <liujiezr@foxmail>
2018-08-08 14:54:52 +0800

    :construction: add 个人页面 编辑资料

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,3 +3,17 @@
 function route_class() {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+if ( !function_exists('pp')) {
+    /**
+     * 调试用
+     *
+     * @param $v
+     */
+    function pp($v)
+    {
+        echo '<pre>';
+        print_r($v);
+        echo '</pre>';
+    }
+}
