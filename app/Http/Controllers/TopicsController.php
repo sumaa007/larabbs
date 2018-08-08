dd52642da0493e4baaf5d2dea60e6dbcc9a5b660
sumaa007 <liujiezr@foxmail>
2018-08-08 18:48:44 +0800

    帖子列表排序

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,12 +14,11 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
-	{
-//		$topics = Topic::paginate(30);
-        $topics = Topic::with('user', 'category')->paginate(10);
-		return view('topics.index', compact('topics'));
-	}
+    public function index(Request $request, Topic $topic)
+    {
+        $topics = $topic->withOrder($request->order)->paginate(20);
+        return view('topics.index', compact('topics'));
+    }
 
     public function show(Topic $topic)
     {
