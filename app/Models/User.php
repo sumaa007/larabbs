8c3590f9fd5cafa6deac3e0cf28ea5354c77e021
sumaa007 <liujiezr@foxmail>
2018-08-09 14:14:52 +0800

    消息通知

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,25 +4,26 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Auth;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable { notify as protected laravelNotify; }
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if ($this->id == Auth::id()) {
+            return;
+        }
+        $this->increment('notification_count');
+        $this->laravelNotify($instance);
+    }
+
     protected $fillable = [
         'name', 'email', 'password', 'introduction', 'avatar'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
@@ -40,5 +41,12 @@ class User extends Authenticatable
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
     }
 }
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,12 +3,12 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
-class ReplyObserver
-{
+class ReplyObserver {
     public function creating(Reply $reply)
     {
         $reply->content = clean($reply->content, 'user_topic_body');
@@ -16,6 +16,10 @@ class ReplyObserver
 
     public function created(Reply $reply)
     {
-        $reply->topic->increment('reply_count', 1);
+        $topic = $reply->topic;
+        $topic->increment('reply_count', 1);
+
+        // 通知作者话题被回复了
+        $topic->user->notify(new TopicReplied($reply));
     }
 }
